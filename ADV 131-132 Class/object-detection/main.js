f494d4dbdacfdb2ec3e8c6f2acf9e05dfbcfb386
Python Tinkerer <87096212+ProOfPython@users.noreply.github.com>
2023-04-28 17:26:22 -0700

    Add files via upload

--- ADV 131-132 Class/object-detection/main.js
+++ ADV 131-132 Class/object-detection/main.js
@@ -1,34 +1,64 @@
+data = []
 img = ''
 label = 0
+paints = []
+secs = ['00', '77', 'FF']
 stat = false
-data = [] /* 2D */
-paints = ['00', '77', 'FF']
+Xs = []
 
-function modelLoaded(){console.log('Model is loaded.'); stat = true; objDetect.detect(img, gotResult)}
+function modelLoaded(){console.log('Model is loaded.'); stat = true;}
 function gotResult(error, results){if (error){console.log(error)}; console.log(results); data = results}
-function preload(){img = loadImage('catNdog.jpg')}
-function setup(){canvas = createCanvas(640, 420); canvas.center(); objDetect = ml5.objectDetector('cocossd', modelLoaded);}
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(380, 380)
+    canvas.center();
+    
+    video = createCapture(VIDEO)
+    video.size(380, 380)
+    video.hide()
+}
+
+function start(){
+    objDetect = ml5.objectDetector('cocossd', modelLoaded)
+}
 
 function getPaint(){
     paint = '#'
     for (i = 0; i < 3; i++){
-        part = paints[round(Math.random() * 100 / 33)]
+        part = secs[round(Math.random() * 100 / 33)]
         paint += part
-        console.log(part)
     }
     return paint
 }
 
 function draw(){
-    image(img, 0, 0, 640, 420)
+    image(video, 0, 0, 380, 380)
     if (stat == true) {
+        objDetect.detect(video, gotResult)
         for (i = 0; i < data.length; i++){
-            paint = getPaint()
-            fill(paint)
-            stroke(paint)
+            x = data[i].x
+            y = data[i].y
+            w = data[i].width
+            h = data[i].height * 0.875
+            c = data[i].confidence
+            l = data[i].label
+
+            r, g, b = random(255)
+            
+            if (Xs.includes(x)){
+                paint = paints[Xs.indexOf(x)]
+            } else {
+                Xs.push(x)
+                paint = getPaint()
+                paints.push(paint)
+            }
+            
+            fill(r, g, b)
+            stroke(r, g, b)
             noFill()
-            rect(data[i].x, data[i].y, data[i].width, data[i].height)
-            text(data[i].label + ' (' + (floor(data[i].confidence * 100)) + '%)', data[i].x + 15, data[i].y + 20)
+            rect(x, y, w, h)
+            text(l + ' (' + (floor(c * 100)) + '%)', x + 15, y + 20)
         }
     }
 }
